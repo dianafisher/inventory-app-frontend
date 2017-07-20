f6267f3cf5d736924fb358fe593ee1a4c56c729f
Diana Fisher <diana.fisher@gmail.com>
2017-07-19 17:46:04 -0700

    Add images for styling.  Add form to AddItem componenent

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -11,29 +11,38 @@ class AddItem extends Component {
   render() {
     return (
       <div>
-        <div className='card-block'>
-          <form onSubmit={this.handleSubmit} className='form-horizontal'>
-            <fieldset>
-              <div className='form-group'>
-                <label className='col-md-2 control-label' for='ms-form-title'>Title</label>
-                <div className='col-md-9'>
-                  <input className='form-control' type='text' id='ms-form-title' name='title' placeholder='Title' />
-                </div>
+        <div className='container'>
+          <div className='row'>
+            <div className='card card-primary animated fadeInUp'>
+              <div className='ms-hero-bg-primary ms-hero-img-mountain'>
+                <h2 className='text-center no-m pt-4 pb-4 color-white index-1'>New Item</h2>
               </div>
-              <div className='form-group'>
-                <label className='col-md-2 control-label' for='ms-form-image'>Image URL</label>
-                <div className='col-md-9'>
-                  <input className='form-control' type='text' name='imageUrl' placeholder='Image URL' />
-                </div>
+              <div className='card-block'>
+                <form onSubmit={this.handleSubmit} className='form-horizontal'>
+                  <fieldset>
+                    <div className='form-group'>
+                      <label className='col-md-2 control-label' htmlFor='ms-form-title'>Title</label>
+                      <div className='col-md-9'>
+                        <input className='form-control' type='text' id='ms-form-title' name='title' placeholder='Title' />
+                      </div>
+                    </div>
+                    <div className='form-group'>
+                      <label className='col-md-2 control-label' htmlFor='ms-form-image'>Image URL</label>
+                      <div className='col-md-9'>
+                        <input className='form-control' type='text' name='imageUrl' placeholder='Image URL' />
+                      </div>
+                    </div>
+                    <div className='form-group'>
+                      <div className='col-md-9 col-md-offset-2'>
+                        <button type='submit' className='btn btn-raised btn-primary'>Submit</button>
+                        <button type='submit' className='btn btn-raised btn-default'>Cancel</button>
+                      </div>
+                    </div>
+                  </fieldset>
+                </form>
               </div>
-              <div className='form-group'>
-                <div className='col-md-9 col-md-offset-2'>
-                  <button type='submit' className='btn btn-raised btn-primary'>Submit</button>
-                  <button type='submit' className='btn btn-raised btn-default'>Cancel</button>
-                </div>
-              </div>
-            </fieldset>
-          </form>
+            </div>
+          </div>
         </div>
       </div>
 
