d666104ad54bc87e8eee593ae4467d2cc2f6d3f6
Diana Fisher <diana.fisher@gmail.com>
2017-08-09 14:59:05 -0700

    Update styling on Login and Register pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,77 +29,61 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <div className="ms-hero-page-override ms-hero-img-city ms-hero-bg-dark-light">
-          <div className="container">
-            <div className="text-center">
-              {/* <span className="ms-logo ms-logo-lg ms-logo-white center-block mb-2 mt-2 animated zoomInDown animation-delay-5">M</span>
-                <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Material
-                <span>Style</span>
-                </h1>
-                <p className="lead lead-lg color-white text-center center-block mt-2 mw-800 text-uppercase fw-300 animated fadeInUp animation-delay-7">Discover our projects and the
-              <span className="color-warning">rigorous process</span> of creation. Our principles are creativity, design, experience and knowledge.</p> */}
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-md-offset-3">
-              <div className="card card-hero card-primary animated fadeInUp animation-delay-4">
-                <div className="card-block">
-                  <h1 className="color-primary text-center">Login</h1>
-                  <form className="form-horizontal" onSubmit={this.handleSubmit}>
-                    <fieldset>
-                      <div className="form-group">
-                        <label htmlFor="inputEmail" className="col-md-2 control-label">Email</label>
-                        <div className="col-md-10">
-                          <input
-                            type="email"
-                            className="form-control"
-                            id="inputEmail"
-                            placeholder="Email"
-                            name='email'
-                            onChange={this.handleInputChange}
-                          />
-                        </div>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="inputPassword" className="col-md-2 control-label">Password</label>
-                        <div className="col-md-10">
-                          <input
-                            type="password"
-                            className="form-control"
-                            id="inputPassword"
-                            placeholder="Password"
-                            name='password'
-                            onChange={this.handleInputChange}
-                          />
-                        </div>
-                      </div>
-                    </fieldset>
-                    <button className="btn btn-raised btn-primary btn-block">Login
-                      <i className="zmdi zmdi-long-arrow-right no-mr ml-1"></i>
-                    </button>
-                  </form>
-                  {/* <div className="text-center mt-4">
-                    <h3>Login with</h3>
-                    <a href="#" className="btn-circle btn-facebook">
-                      <i className="zmdi zmdi-facebook"></i>
-                    </a>
-                    <a href="#" className="btn-circle btn-twitter">
-                      <i className="zmdi zmdi-twitter"></i>
-                    </a>
-                    <a href="#" className="btn-circle btn-google">
-                      <i className="zmdi zmdi-google"></i>
-                    </a>
-                  </div> */}
-                </div>
-              </div>
+      <div className="bg-full-page ms-hero-img-robots ms-hero-bg-primary">
+        <div className='mw-500 absolute-center center'>
+          <div className="card card-hero card-primary animated fadeInUp animation-delay-4">
+            <div className="card-block">
+              <h1 className="color-primary text-center">Login</h1>
+              <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                <fieldset>
+                  <div className="form-group">
+                    <label htmlFor="inputEmail" className="col-md-2 control-label">Email</label>
+                    <div className="col-md-10">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="Email"
+                        name='email'
+                        onChange={this.handleInputChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputPassword" className="col-md-2 control-label">Password</label>
+                    <div className="col-md-10">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="inputPassword"
+                        placeholder="Password"
+                        name='password'
+                        onChange={this.handleInputChange}
+                      />
+                    </div>
+                  </div>
+                </fieldset>
+                <button className="btn btn-raised btn-primary btn-block">Login
+                  <i className="zmdi zmdi-long-arrow-right no-mr ml-1"></i>
+                </button>
+              </form>
+              {/* <div className="text-center mt-4">
+                  <h3>Login with</h3>
+                  <a href="#" className="btn-circle btn-facebook">
+                  <i className="zmdi zmdi-facebook"></i>
+                  </a>
+                  <a href="#" className="btn-circle btn-twitter">
+                  <i className="zmdi zmdi-twitter"></i>
+                  </a>
+                  <a href="#" className="btn-circle btn-google">
+                  <i className="zmdi zmdi-google"></i>
+                  </a>
+              </div> */}
             </div>
           </div>
         </div>
       </div>
-    )
+      )
   }
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -31,69 +31,58 @@ class Register extends Component {
   render() {
     return (
       <div>
-        <div className="ms-hero-page-override ms-hero-img-room ms-bg-fixed ms-hero-bg-dark-light">
-          <div className="container">
-            <div className="text-center">
-              <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Create Account</h1>
-              {/* <p className="lead lead-lg color-light text-center center-block mt-2 mw-800 text-uppercase fw-300 animated fadeInUp animation-delay-7">Do not wait more register now! Access our great community and benefit from
-              <span className="color-info">exclusive membership</span> conditions.</p> */}
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="card card-primary card-hero animated fadeInUp animation-delay-4">
-            <div className="card-block">
-              <form className="form-horizontal" onSubmit={this.handleSubmit}>
-                <fieldset>
-                  <div className="form-group">
-                    <label htmlFor="inputEmail" className="col-md-2 control-label">Email</label>
-                    <div className="col-md-9">
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="inputEmail"
-                        placeholder="Email"
-                        name='email'
-                        onChange={this.handleInputChange}
-                      />
+        <div className="bg-full-page ms-hero-img-robots ms-hero-bg-primary">
+          <div className="mw-500 absolute-center center">
+            <div className="card card-primary card-hero animated fadeInUp animation-delay-4">
+              <div className="card-block">
+                <h1 className='color-primary text-center'>Create Account</h1>
+                <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                  <fieldset>
+                    <div className="form-group">
+                      <label htmlFor="inputEmail" className="col-md-2 control-label">Email</label>
+                      <div className="col-md-9">
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="inputEmail"
+                          placeholder="Email"
+                          name='email'
+                          onChange={this.handleInputChange}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="inputName" className="col-md-2 control-label">Name</label>
-                    <div className="col-md-9">
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="inputName"
-                        placeholder="Name"
-                        name='name'
-                        onChange={this.handleInputChange}
-                      />
+                    <div className="form-group">
+                      <label htmlFor="inputName" className="col-md-2 control-label">Name</label>
+                      <div className="col-md-9">
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="inputName"
+                          placeholder="Name"
+                          name='name'
+                          onChange={this.handleInputChange}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="inputPassword" className="col-md-2 control-label">Password</label>
-                    <div className="col-md-9">
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="inputPassword"
-                        placeholder="Password"
-                        name='password'
-                        onChange={this.handleInputChange}
-                      />
+                    <div className="form-group">
+                      <label htmlFor="inputPassword" className="col-md-2 control-label">Password</label>
+                      <div className="col-md-9">
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="inputPassword"
+                          placeholder="Password"
+                          name='password'
+                          onChange={this.handleInputChange}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-4 col-md-offset-8">
-                      <button
-                        className="btn btn-raised btn-primary btn-block mt-4"
-                        type='submit'
-                      >Register Now</button>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
+                  </fieldset>
+                  <button className="btn btn-raised btn-primary btn-block">CREATE ACCOUNT
+                    <i className="zmdi zmdi-long-arrow-right no-mr ml-1"></i>
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
