12678b9c1c8ca6eee552cce543a4998f6740e25b
Diana Fisher <diana.fisher@gmail.com>
2017-08-17 21:37:13 -0700

    Adjust styling of Register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,7 +10,8 @@ class Register extends Component {
     this.state = {
       email: '',
       name: '',
-      password: ''
+      password: '',
+      hasFocus: 'none'
     };
   }
 
@@ -23,9 +24,101 @@ class Register extends Component {
 
   handleInputChange = (e) => {
     const target = e.target;
-    console.log(target.type);
     const name = target.name;
-    console.log(name);
+    // console.log(name);
+    const value = target.value;
+    // console.log(value);
+    this.setState({[name] : value});
+  }
+
+  _onFocus = (e) => {
+    const target = e.target;
+    // console.log(target);
+    const name = target.name;
+    // console.log(name, 'has focus');
+    this.setState({ 'hasFocus': name });
+  }
+
+  _onBlur = (e) => {
+    const target = e.target;
+    const name = target.name;
+    // console.log(name, 'lost focus');
+    this.setState({ 'hasFocus': 'none' })
+  }
+
+  _renderEmailField = () => {
+    let className = this._classNameForField('email');
+
+    return (
+      <div className={className}>
+        <label className="control-label" htmlFor="inputEmail">Email</label>
+        <input
+          className="form-control"
+          id="inputEmail"
+          type="text"
+          name='email'
+          required="required"
+          onChange={this.handleInputChange}
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
+        ></input>
+        <p className="help-block">Please provide a valid email address.</p>
+      </div>
+    )
+  }
+
+  _renderNameField = () => {
+    let className = this._classNameForField('name');
+
+    return (
+      <div className={className}>
+        <label htmlFor="inputName" className="control-label">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="inputName"
+          name='name'
+          required="required"
+          onChange={this.handleInputChange}
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
+        />
+        <p className="help-block">Please provide a name.</p>
+      </div>
+    )
+  }
+
+  _renderPasswordField = () => {
+    let className = this._classNameForField('password');
+
+    return (
+      <div className={className}>
+        <label htmlFor="inputPassword" className="control-label">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="inputPassword"
+          name='password'
+          required="required"
+          onChange={this.handleInputChange}
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
+        />
+        <p className="help-block">Please provide a password.</p>
+      </div>
+    )
+  }
+
+  _classNameForField = (name)  => {
+    let value = this.state[name];
+    let className = 'form-group label-floating';
+    if (value.length === 0) {
+      className = 'form-group label-floating is-empty';
+    }
+    if (this.state.hasFocus === name) {
+      className = 'form-group label-floating is-focused';
+    }
+    return className;
   }
 
   render() {
@@ -37,46 +130,10 @@ class Register extends Component {
               <div className="card-block">
                 <h1 className='color-primary text-center'>Create Account</h1>
                 <form className="form-horizontal" onSubmit={this.handleSubmit}>
-                  <fieldset>
-                    <div className="form-group">
-                      <label htmlFor="inputEmail" className="col-md-2 control-label">Email</label>
-                      <div className="col-md-9">
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="inputEmail"
-                          placeholder="Email"
-                          name='email'
-                          onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="inputName" className="col-md-2 control-label">Name</label>
-                      <div className="col-md-9">
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="inputName"
-                          placeholder="Name"
-                          name='name'
-                          onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="inputPassword" className="col-md-2 control-label">Password</label>
-                      <div className="col-md-9">
-                        <input
-                          type="password"
-                          className="form-control"
-                          id="inputPassword"
-                          placeholder="Password"
-                          name='password'
-                          onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </div>
+                  <fieldset style={styles.fieldset}>
+                    { this._renderEmailField() }
+                    { this._renderNameField() }
+                    { this._renderPasswordField() }
                   </fieldset>
                   <button className="btn btn-raised btn-primary btn-block">CREATE ACCOUNT
                     <i className="zmdi zmdi-long-arrow-right no-mr ml-1"></i>
@@ -93,6 +150,12 @@ class Register extends Component {
 
 Register.propTypes = {
   onRegisterUser: PropTypes.func.isRequired
+}
+
+const styles = {
+  fieldset: {
+    marginBottom: '40px'
+  },
 }
 
 export default Register;
